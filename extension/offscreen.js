7deb89a7458c838ae89bf32bdbc0bb7310feca45
Robert <rspier@pobox.com>
2023-12-09 15:47:58 -0800

    start of support for on-demand battery updates

--- extension/offscreen.js
+++ extension/offscreen.js
@@ -1,16 +1,15 @@
 
 // Set up the battery event handlers.
 navigator.getBattery().then(function (battery) {
-  battery.addEventListener('chargingchange', function () { updateBatteryStatus(battery); })
-  battery.addEventListener('levelchange', function () { updateBatteryStatus(battery); })
-  battery.addEventListener('chargingtimechange', function () { updateBatteryStatus(battery); })
-  battery.addEventListener('dischargingtimechange', function () { updateBatteryStatus(battery); })
+  battery.addEventListener('chargingchange', function () { batteryChanged(battery); })
+  battery.addEventListener('levelchange', function () { batteryChanged(battery); })
+  battery.addEventListener('chargingtimechange', function () { batteryChanged(battery); })
+  battery.addEventListener('dischargingtimechange', function () { batteryChanged(battery); })
   // Send initial data
-  updateBatteryStatus(battery);
+  batteryChanged(battery);
 });
 
-
-function updateBatteryStatus(battery) {
+function batteryChanged(battery) {
   sendToBackground("battery",
     {
       charging: battery.charging,
@@ -27,4 +26,20 @@ function sendToBackground(type, data) {
     target: 'background',
     data: data
   });
+}
+
+chrome.runtime.onMessage.addListener(handleMessages);
+async function handleMessages(message) {
+    // Return early if this message isn't meant for the offscreen script
+    if (message.target !== 'offscreen') {
+        return;
+    }
+    // Dispatch the message to an appropriate handler.
+    switch (message.type) {
+        case 'updateBatteryState':
+          navigator.getBattery().then( (b) => batteryChanged(b) );
+            break;
+        default:
+            console.warn(`Unexpected message type received: '${message.type}'.`);
+    }
 }
